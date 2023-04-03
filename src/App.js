df56e2b171ba510f565cad03979af38a16bc3fb1
Muhammad <umedjon02.ud@gmail.com>
2023-04-04 03:34:06 +0500

    Add the todo-app for review

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+
+  const updatedTodo = (todoId, newValue) => {
+    setTodos((prev) => prev.map((item) => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+  return (
+    <div>
+      <h1>What is the plan for today</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updatedTodo={updatedTodo}
+      />
+    </div>
+  );
+}
+
+export default TodoList;
